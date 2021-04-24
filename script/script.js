479b71a0312e44244d855a8cd97c26a5f32668a2
timnorov <78200592+timnorov@users.noreply.github.com>
2021-04-24 13:34:27 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -40,34 +40,39 @@ window.addEventListener('DOMContentLoaded', () => {
 
         const btnMenu = document.querySelector('.menu'),
             menu = document.querySelector('menu'),
-            menuItems = menu.querySelectorAll('ul>li');
+            menuItems = menu.querySelectorAll('ul>li'),
+            body = document.querySelector('body');
 
-        btnMenu.addEventListener('click', () => {
-            if (window.screen.width >= 768) {
-                menu.classList.toggle('active-menu');
-            } else {
-                if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {
-                    menu.style.transform = `translate(100%)`;
-                } else {
-                    menu.style.transform = `translate(-100%)`;
-                }
-            }
-        });
-
-        menu.addEventListener('click', event => {
+        body.addEventListener('click', () => {
             let target = event.target;
 
-            if (!target.classList.contains('active-menu')) {
+            if (target.closest('.menu')) {
+                if (window.screen.width >= 768) {
+                    menu.classList.toggle('active-menu');
+                } else {
+                    if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {
+                        menu.style.transform = `translate(100%)`;
+                    } else {
+                        menu.style.transform = `translate(-100%)`;
+                    }
+                }
+            } else if (target.closest('.active-menu')) {
+
+                if (!target.classList.contains('active-menu') && !target.classList.contains('close-btn')) {
+                    menu.classList.remove('active-menu');
+                    target = event.menuItems;
+                    event.preventDefault();
+
+                    const blockID = event.target.getAttribute('href').substr(1);
+                    document.getElementById(blockID).scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start',
+                    });
+                } else if (target.classList.contains('close-btn')) {
+                    menu.classList.remove('active-menu');
+                }
+            } else {
                 menu.classList.remove('active-menu');
-                target = event.menuItems;
-                event.preventDefault();
-
-                const blockID = event.target.getAttribute('href').substr(1);
-
-                document.getElementById(blockID).scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'start',
-                });
             }
         });
     };
@@ -106,7 +111,7 @@ window.addEventListener('DOMContentLoaded', () => {
         //popup animation
         let count = 0,
             popupInterval;
-            
+
         const popupAnimate = function() {
             popupInterval = requestAnimationFrame(popupAnimate);
             count += 3;
