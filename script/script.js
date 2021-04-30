b39365fc2f4eb0dc3f1bf563ca81d552f57c762b
timnorov <timur.norov@gmail.com>
2021-04-30 15:51:55 +0300

    Submitted lesson24

--- script/script.js
+++ script/script.js
@@ -287,7 +287,7 @@ window.addEventListener('DOMContentLoaded', () => {
         event.target.src = event.target.dataset.img.slice(0, -5)+ '.jpg'))
 
     //калькулятор
-    const input = document.querySelectorAll('.calc-item');
+    const input = document.querySelectorAll('input.calc-item');
 
     input.forEach(input => input.addEventListener('input', () => 
         input.value = input.value.replace(/\D/g, '')))
@@ -351,4 +351,63 @@ window.addEventListener('DOMContentLoaded', () => {
             formPhone.value = formPhone.value.replace(/[-]+/g, '-')
             formPhone.value = formPhone.value.replace(/^\-+|\-+$/g, '')
         });
+
+    //калькулятор
+
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
+        let oldTotal = 0;
+
+            const countSum = () => {
+                let total = 0,
+                    countValue = 1,
+                    dayValue = 1;
+                const typeValue = calcType.options[calcType.selectedIndex].value,
+                    squareValue = +calcSquare.value;
+
+                if (calcCount.value > 1){
+                    countValue += (calcCount.value - 1) / 10;
+                }
+                
+                if (calcDay.value && calcDay.value < 5) {
+                    dayValue *= 2;
+                } else if (calcDay.value && calcDay.value < 10){
+                    dayValue *= 1.5;
+                }
+
+                if (typeValue && squareValue) {
+                    total = price * typeValue * squareValue * countValue * dayValue;
+                } 
+                animateValue(totalValue, oldTotal, total, 600);
+                oldTotal = total;
+                totalValue.textContent = total;
+            };
+
+            calcBlock.addEventListener('change', (event) => {
+                const target = event.target;
+
+                if (target.matches('select') || target.matches('input')) {
+                    countSum();
+                }
+            });
+            function animateValue(totalValue, start, end, duration) {
+                let startTimestamp = null;
+                const step = (timestamp) => {
+                if (!startTimestamp) startTimestamp = timestamp;
+                const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+                totalValue.textContent = Math.floor(progress * (end - start) + start);
+                if (progress < 1) {
+                window.requestAnimationFrame(step);
+                }
+            };
+            window.requestAnimationFrame(step);
+            }
+    };
+
+    calc(100);
 }); 
