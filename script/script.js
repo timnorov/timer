95d3dde857e08baffb33ce2fe1081d7ad0caaf1b
timnorov <timur.norov@gmail.com>
2021-05-06 13:06:01 +0300

    Submitted lesson29

--- script/script.js
+++ script/script.js
@@ -485,12 +485,13 @@ window.addEventListener('DOMContentLoaded', () => {
             </div>
             `;
             const formData = new FormData(form);
-            let body = {};
-            formData.forEach((val, key) => {
-                body[key] = val;
-            });
-            postData(body)
-                .then(() => {
+        
+            postData(formData)
+                .then((response) => {
+                    if (response.status !== 200) {
+                        throw new Error('status error not 200')
+                    }
+                    // console.log(response);
                     statusMessage.textContent = successMessage;
                     form.reset();
                     formName1.classList.remove('success');
@@ -499,32 +500,23 @@ window.addEventListener('DOMContentLoaded', () => {
                     setTimeout(() => {
                         form.removeChild(statusMessage)
                     }, 5000);
-                },(error) => {
+                })
+                .catch((error) => {
                     statusMessage.textContent = errorMessage;
                     console.error(error);
                     setTimeout(() => {
                         form.removeChild(statusMessage)
                     }, 5000);
-                })
+                });
         });
 
-        const postData = (body) => {
-            
-            return new Promise((resolve, reject) => {
-                const request = new XMLHttpRequest();
-                request.addEventListener('readystatechange', () => {
-                    if(request.readyState !== 4) {
-                        return;
-                    }
-                    if (request.status === 200) {
-                        resolve();
-                    } else {
-                        reject(request.status);
-                    }
-                });
-                request.open('POST', './server.php');
-                request.setRequestHeader('Content-Type', 'application/json');
-                request.send(JSON.stringify(body));
+        const postData = (formData) => {
+            return fetch('./server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: formData
             });
         };
     };
@@ -561,12 +553,13 @@ window.addEventListener('DOMContentLoaded', () => {
             </div>
             `;
             const formData = new FormData(form);
-            let body = {};
-            formData.forEach((val, key) => {
-                body[key] = val;
-            });
-            postData(body)
-                .then(() => {
+        
+            postData(formData)
+                .then((response) => {
+                    if (response.status !== 200) {
+                        throw new Error('status error not 200')
+                    }
+                    // console.log(response);
                     statusMessage.textContent = successMessage;
                     form.reset();
                     formName3.classList.remove('success');
@@ -575,32 +568,23 @@ window.addEventListener('DOMContentLoaded', () => {
                     setTimeout(() => {
                         form.removeChild(statusMessage)
                     }, 5000);
-                },(error) => {
+                })
+                .catch((error) => {
                     statusMessage.textContent = errorMessage;
                     console.error(error);
                     setTimeout(() => {
                         form.removeChild(statusMessage)
                     }, 5000);
-                })
+                });
         });
 
-        const postData = (body) => {
-            
-            return new Promise((resolve, reject) => {
-                const request = new XMLHttpRequest();
-                request.addEventListener('readystatechange', () => {
-                    if(request.readyState !== 4) {
-                        return;
-                    }
-                    if (request.status === 200) {
-                        resolve();
-                    } else {
-                        reject(request.status);
-                    }
-                });
-                request.open('POST', './server.php');
-                request.setRequestHeader('Content-Type', 'application/json');
-                request.send(JSON.stringify(body));
+        const postData = (formData) => {
+            return fetch('./server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: formData
             });
         };
     };
@@ -638,12 +622,13 @@ window.addEventListener('DOMContentLoaded', () => {
             </div>
             `;
             const formData = new FormData(form);
-            let body = {};
-            formData.forEach((val, key) => {
-                body[key] = val;
-            });
-            postData(body)
-                .then(() => {
+        
+            postData(formData)
+                .then((response) => {
+                    if (response.status !== 200) {
+                        throw new Error('status error not 200')
+                    }
+                    // console.log(response);
                     statusMessage.textContent = successMessage;
                     form.reset();
                     formName.classList.remove('success');
@@ -653,32 +638,23 @@ window.addEventListener('DOMContentLoaded', () => {
                     setTimeout(() => {
                         form.removeChild(statusMessage)
                     }, 5000);
-                },(error) => {
+                })
+                .catch((error) => {
                     statusMessage.textContent = errorMessage;
                     console.error(error);
                     setTimeout(() => {
                         form.removeChild(statusMessage)
                     }, 5000);
-                })
+                });
         });
 
-        const postData = (body) => {
-            
-            return new Promise((resolve, reject) => {
-                const request = new XMLHttpRequest();
-                request.addEventListener('readystatechange', () => {
-                    if(request.readyState !== 4) {
-                        return;
-                    }
-                    if (request.status === 200) {
-                        resolve();
-                    } else {
-                        reject(request.status);
-                    }
-                });
-                request.open('POST', './server.php');
-                request.setRequestHeader('Content-Type', 'application/json');
-                request.send(JSON.stringify(body));
+        const postData = (formData) => {
+            return fetch('./server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: formData
             });
         };
     };
